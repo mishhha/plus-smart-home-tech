9ee0c3ffd60b2a12cae7bcd30da721126faff6c5
Misha Plehanov <mih.plekhanov@yandex.ru>
2026-08-07 12:53:23 +0300

    реализация проекта 19 спринта. модуль Collector.

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/dto/sensor/MotionSensorEventDto.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.collector.dto.sensor;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MotionSensorEventDto implements SensorEventDto {
+    private String id;
+    @JsonProperty("hubId") private String hubId;
+    private Instant timestamp;
+    @JsonProperty("linkQuality") private Integer linkQuality;
+    private Boolean motion;
+    private Integer voltage;
+    private String type;
+}
