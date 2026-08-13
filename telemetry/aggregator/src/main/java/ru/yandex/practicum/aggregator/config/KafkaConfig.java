44601d1c762b208353ef8be23b06eafaa7935034
Misha Plehanov <mih.plekhanov@yandex.ru>
2026-08-13 14:02:46 +0300

    refactor(aggregator): конфигурация Kafka вынесена в @ConfigurationProperties

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/config/KafkaConfig.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/config/KafkaConfig.java
@@ -1,12 +1,12 @@
 package ru.yandex.practicum.aggregator.config;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
@@ -17,34 +17,30 @@ import ru.yandex.practicum.serialization.SensorEventDeserializer;
 import java.util.Properties;
 
 @Configuration
+@RequiredArgsConstructor
 public class KafkaConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
-    @Value("${aggregator.kafka.consumer.group-id}")
-    private String groupId;
+    private final KafkaProperties kafkaProperties;
 
     @Bean(destroyMethod = "close")
     public KafkaConsumer<String, SensorEventAvro> sensorEventConsumer() {
+        KafkaProperties.Consumer consumerProps = kafkaProperties.getConsumer();
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerProps.getGroupId());
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class.getName());
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false"); // ручной commit
-
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, consumerProps.getAutoOffsetReset());
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, String.valueOf(consumerProps.isEnableAutoCommit()));
         return new KafkaConsumer<>(props);
     }
 
     @Bean(destroyMethod = "close")
     public KafkaProducer<String, SensorsSnapshotAvro> snapshotProducer() {
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class.getName());
-
         return new KafkaProducer<>(props);
     }
 }
