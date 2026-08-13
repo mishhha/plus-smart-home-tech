ebde93937c30e1f6793ae298fd1187bfcf15a605
Misha Plehanov <mih.plekhanov@yandex.ru>
2026-08-13 15:37:43 +0300

    fix(analyzer, agreggator) Внесение правок после ревью.

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/AggregationStarter.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/AggregationStarter.java
@@ -9,7 +9,6 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.errors.WakeupException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.aggregator.config.KafkaProperties;
 import ru.yandex.practicum.aggregator.service.SnapshotService;
